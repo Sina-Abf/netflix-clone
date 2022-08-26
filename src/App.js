cb31b6ee87aa74752136199db5ca8716c77f6799
Sina-Abf <abf.sina@gmail.com>
2022-08-26 13:46:48 +0430

    sign up functionality and fix env file bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import { AuthContextProvider } from './context/AuthContext';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Account from './pages/Account.jsx';
 
 function App() {
   return (
@@ -9,6 +11,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/account" element={<Account />} />
       </Routes>
     </>
   );
