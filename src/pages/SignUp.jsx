b535e36e887c7ebd2a280d6fb53e0fa501f8cfe9
Sina-Abf <abf.sina@gmail.com>
2022-08-26 14:05:55 +0430

    handle signup and signout logic

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { user, signUp } = UserAuth();
+  const navigate = useNavigate();
 
   const submitHandler = async (e) => {
     e.preventDefault();
 
     try {
       await signUp(email, password);
+      navigate('/');
     } catch (err) {
       console.log(err);
     }
